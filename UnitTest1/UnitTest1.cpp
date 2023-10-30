af495123e04e06042b153e4841323b6f48b7d44c
Maksym Cherniienko <h0nza4ok@gmail.com>
2023-10-30 23:27:41 +0200

    final

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../Lab_06_1/Lab_06_1.cpp"
+#include "../Lab_06_1/Lab_06_1_rec.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,9 +12,11 @@ namespace UnitTest1
 		
 		TEST_METHOD(TestMethod1)
 		{
-			const int n = 22;
-			int a[n];
-			int S = Sum(a, n, 0);
+			const int n = 10;
+			int a[n] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			int S = 55;
+
+			int result = Sum(a, n, 0);
 
 			Assert::AreEqual(Sum(a, n, 0), S, 1e-1);
 		}
